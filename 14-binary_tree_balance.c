40f502d9df06b51a9f5cc743dd90c8c612fcb7b3
afro <aigbemanuel@gmail.com>
2023-10-30 21:22:46 -0700

    aadded 14th tasks

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: The height of the tree starting at @node
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+    size_t height_l;
+    size_t height_r;
+
+    height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+    height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+    return (height_l > height_r ? height_l : height_r);
+}
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    int left_height, right_height;
+
+    if (tree == NULL)
+        return (0);
+
+    left_height = binary_tree_height(tree->left);
+    right_height = binary_tree_height(tree->right);
+
+    return (left_height - right_height);
+}
